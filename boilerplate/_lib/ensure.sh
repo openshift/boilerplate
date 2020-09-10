29d4e36dd82109cf5bcd5c4e244ae503c025a557
Eric Fried <efried@redhat.com>
2020-09-10 17:03:04 -0500

    `operator-sdk generate` at the proper version
    Enhances ensure.sh to recognize the `operator-sdk` directive. This
    discovers the version of operator-sdk used by the consuming repository
    (from go.mod) and makes it available via a symlink at
    `./.operator-sdk/bin/operator-sdk`. If there's already one installed in
    your `$PATH`, and it's the right version, we just symlink to it.
    Otherwise we download the appropriate binary.
    
    Adds a script to boilerplate/_lib called `operator-sdk-generate.sh` that
    uses the above, and then executes the `operator-sdk generate` commands
    appropriate to the version.
    
    Swaps out the guts of `make opgenerate` to invoke the above.

--- boilerplate/_lib/ensure.sh
+++ boilerplate/_lib/ensure.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+source ${0%/*}/common.sh
+
 GOLANGCI_LINT_VERSION="1.30.0"
 DEPENDENCY=${1:-}
 GOOS=$(go env GOOS)
 
 case "${DEPENDENCY}" in
+
 golangci-lint)
     GOPATH=$(go env GOPATH)
     if which golangci-lint ; then
@@ -22,6 +25,54 @@ golangci-lint)
         curl -sfL "${DOWNLOAD_URL}" | tar -C "${GOPATH}/bin" -zx --strip-components=1 "golangci-lint-${GOLANGCI_LINT_VERSION}-${GOOS}-amd64/golangci-lint"
     fi
     ;;
+
+operator-sdk)
+    #########################################################
+    # Ensure operator-sdk is installed at the desired version
+    # When done, ./.operator-sdk/bin/operator-sdk will be a
+    # symlink to the appropriate executable.
+    #########################################################
+    # First discover the desired version from go.mod
+    # The following properly takes `replace` directives into account.
+    wantver=$(go list -json -m github.com/operator-framework/operator-sdk | jq -r 'if .Replace != null then .Replace.Version else .Version end')
+    echo "go.mod says you want operator-sdk $wantver"
+    # Where we'll put our (binary and) symlink
+    mkdir -p .operator-sdk/bin
+    cd .operator-sdk/bin
+    # Discover existing, giving preference to one already installed in
+    # this path, because that has a higher probability of being the
+    # right one.
+    if [[ -x ./operator-sdk ]] && [[ "$(osdk_version ./operator-sdk)" == "$wantver" ]]; then
+        echo "operator-sdk $wantver already installed"
+        exit 0
+    fi
+    # Is there one in $PATH?
+    if which operator-sdk && [[ $(osdk_version $(which operator-sdk)) == "$wantver" ]]; then
+        osdk=$(realpath $(which operator-sdk))
+        echo "Found at $osdk"
+    else
+        case "$(uname -s)" in
+            Linux*)
+                binary="operator-sdk-${wantver}-x86_64-linux-gnu"
+                ;;
+            Darwin*)
+                binary="operator-sdk-${wantver}-x86_64-apple-darwin"
+                ;;
+            *)
+                echo "OS unsupported"
+                exit 1
+                ;;
+        esac
+        echo "Downloading $binary"
+        curl -OJL https://github.com/operator-framework/operator-sdk/releases/download/${wantver}/${binary}
+        chmod +x ${binary}
+        osdk=${binary}
+    fi
+    # Create (or overwrite) the symlink to the binary we discovered or
+    # downloaded above.
+    ln -sf $osdk operator-sdk
+    ;;
+
 venv)
     # Set up a python virtual environment
     python3 -m venv .venv
@@ -30,6 +81,7 @@ venv)
         .venv/bin/python3 -m pip install -r "$2"
     fi
     ;;
+
 *)
     echo "Unknown dependency: ${DEPENDENCY}"
     exit 1
