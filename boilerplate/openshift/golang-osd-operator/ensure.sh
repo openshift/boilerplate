0eae558131206e85eea6e228f08b43f80751876a
Eric Fried <efried@redhat.com>
2020-09-16 13:21:58 -0500

    Move ensure.sh into openshift/golang-osd-operator
    This script is used by `make` targets in
    `openshift/golang-osd-operator`, not by boilerplate infrastructure, so
    it makes more sense for it to live in the convention.

--- boilerplate/openshift/golang-osd-operator/ensure.sh
+++ boilerplate/openshift/golang-osd-operator/ensure.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-source ${0%/*}/common.sh
+REPO_ROOT=$(git rev-parse --show-toplevel)
+source $REPO_ROOT/boilerplate/_lib/common.sh
 
 GOLANGCI_LINT_VERSION="1.30.0"
 DEPENDENCY=${1:-}
