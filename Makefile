6dddc7040d9299961a6c538c9cb6b7a05577cdb4
Eric Fried <efried@redhat.com>
2020-10-13 13:45:00 -0500

    Consumers build/test with tagged images
    The openshift/golang-osd-operator convention now syncs the image used by
    prow and appsre by editing build/Dockerfile and .ci-operator.yaml. The
    image tag used in both is the latest `image-v*` tag from the boilerplate
    repository.

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 ALLOW_DIRTY_CHECKOUT?=false
 IMG?=boilerplate
-IMG_TAG?=latest
-QUAY_IMAGE?=quay.io/app-sre/$(IMG)
 CONTAINER_ENGINE?=$(shell command -v podman 2>/dev/null || echo "docker")
-GIT_HASH?=$(shell git rev-parse --short=7 HEAD)
 
 # Tests rely on this starting off unset. (And if it is set, it's usually
 # not for the reasons we care about.)
@@ -13,8 +10,12 @@ unexport REPO_NAME
 isclean:
 	@(test "$(ALLOW_DIRTY_CHECKOUT)" != "false" || test 0 -eq $$(git status --porcelain | wc -l)) || (echo "Local git checkout is not clean, commit changes and try again." >&2 && exit 1)
 
+.PHONY: tag-check
+tag-check:
+	config/tag-check.sh
+
 .PHONY: test
-test: isclean
+test: isclean tag-check
 	test/driver
 
 .PHONY: pr-check
@@ -22,17 +23,11 @@ pr-check: test
 
 .PHONY: docker-build
 docker-build:
-	GIT_HASH=$(shell git rev-parse --short=7 HEAD)
-	cd config; $(CONTAINER_ENGINE) build -t $(IMG):$(IMG_TAG) .	
+	cd config; $(CONTAINER_ENGINE) build -t $(IMG):latest .
 
 .PHONY: docker-push
 docker-push:
-	skopeo copy --dest-creds "$(QUAY_USER):$(QUAY_TOKEN)" \
-	    "docker-daemon:$(IMG):$(IMG_TAG)" \
-	    "docker://$(QUAY_IMAGE):$(IMG_TAG)"
-	skopeo copy --dest-creds "$(QUAY_USER):$(QUAY_TOKEN)" \
-	    "docker-daemon:$(IMG):$(IMG_TAG)" \
-	    "docker://$(QUAY_IMAGE):$(GIT_HASH)"
+	config/push.sh quay.io app-sre $(IMG)
 
 .PHONY: build-push
 build-push: docker-build docker-push
---
+++ config/tag-check.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -xe
+
+# If you mess with the build image, you must publish a new
+# image-v{X}.{Y}.{Z} tag. We can't automatically generate that tag,
+# because we can't figure out what the semver should be, because we
+# don't know the impact of what you've changed. So this script makes
+# sure that you created the tag. And since we're running this in prow,
+# it also makes sure that the tag has been pushed to upstream.
+
+# This gets us the last non-merge commit:
+commit=$(git rev-list --no-merges -n 1 HEAD)
+# If that commit corresponds to an image tag, this gets the tag:
+tag=$(git describe --exact-match --tag --match image-v* $commit 2>/dev/null || true)
+
+if [[ -n "$tag" ]]; then
+    echo "Found tag $tag at current commit :)"
+    exit 0
+fi
+
+# No tag here. That's okay as long as there were no changes to the build
+# image.
+
+# Since we're in a PR, and there may be multiple commits, we want to
+# check all of them; so compare against the last merge commit before
+# this one. This should work both locally (that should correspond to
+# upstream/master) and in CI (which creates a merge commit to test
+# against).
+# NOTE: This assumes we always merge with merge commits.
+last_merge_commit=$(git log --merges -n1 HEAD^ --format=%H)
+if [[ -n "$(git diff $last_merge_commit --name-only config/)" ]]; then
+    echo "Image build configuration has changed!"
+    echo "You must push a new image-v{X}.{Y}.{Z} tag at commit $commit!"
+    echo "See https://github.com/openshift/boilerplate/blob/$commit/README.md#build-images"
+    exit 1
+fi
+
+exit 0
