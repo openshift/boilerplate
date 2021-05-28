2714e7c4a11d931ee8272b55c49445437ce2273a
Eric Fried <efried@redhat.com>
2021-05-28 15:52:59 -0500

    image-v2.1.0: add `gh`
    Add the `gh` (GitHub CLI) executable to the backing image so CICD jobs
    can do consumer reports.
    
    Part of OSD-5962

--- config/build.sh
+++ config/build.sh
@@ -5,6 +5,16 @@
 set -x
 set -euo pipefail
 
-echo NOOP
+####
+# gh
+####
+GH_VERSION=1.10.3
+GH_SHA256SUM="257c5bf641d85606337bc91c6507066a21ba6c849be50231b768fe2fd07517ea"
+GH_LOCATION=https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.tar.gz
+
+curl -L -o /tmp/gh.tgz $GH_LOCATION
+echo ${GH_SHA256SUM} /tmp/gh.tgz | sha256sum -c
+tar -xvzf /tmp/gh.tgz gh_${GH_VERSION}_linux_amd64/bin/gh
+mv gh_${GH_VERSION}_linux_amd64/bin/gh /usr/local/bin
 
 exit 0
