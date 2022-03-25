1c13a6d23ef92603bbae32d5fb8f543789fc579c
Christoph Blecker <cblecker@redhat.com>
2022-03-25 09:04:02 -0700

    Update image tag in test files

--- test/lib.sh
+++ test/lib.sh
@@ -3,7 +3,7 @@ if [ "$BOILERPLATE_SET_X" ]; then
 fi
 
 # NOTE: Change this when publishing a new image tag.
-LATEST_IMAGE_TAG=image-v2.1.0
+LATEST_IMAGE_TAG=image-v2.2.0
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 # Make all tests use this local clone by default.
