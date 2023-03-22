b2e3e1b90c2e5a60020cba909d2112b76021f201
Diego Santamaria <dsantama@redhat.com>
2023-03-22 09:40:39 -0600

    Swap sed command delimiter
    I believe this will explode because $TEST_IMAGE contains /s. I would use some other delimiter for that sed command -- one that can't appear in a URI -- e.g.:
    
    Co-authored-by: Eric Fried <2uasimojo@users.noreply.github.com>
    
    Remove project.mk

--- boilerplate/openshift/golang-osd-operator-acceptance-test/project.mk
+++
@@ -1,6 +0,0 @@
-# Project specific values
-OPERATOR_NAME=$(sed -n 's/.*OperatorName .*"\([^"]*\)".*/\1/p' config/config.go)
-OPERATOR_NAMESPACE=$(sed -n 's/.*OperatorNamespace .*"\([^"]*\)".*/\1/p' config/config.go)
-
-#Test Specific values
-TEST_IMAGE?='quay.io/openshift/origin-tools:latest'
