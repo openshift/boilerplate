748ac78f1280c5cca1d982b5f22ae7350c2030c3
Alice Hubenko <ahubenko@redhat.com>
2024-07-09 12:30:11 -0700

    [OSD-13681] Added a prefix 'g' to handle leading zeroes in CSV version

--- boilerplate/openshift/golang-osd-operator/standard.mk
+++ boilerplate/openshift/golang-osd-operator/standard.mk
@@ -42,7 +42,7 @@ endif
 
 # Generate version and tag information from inputs
 COMMIT_NUMBER=$(shell git rev-list `git rev-list --parents HEAD | grep -E "^[a-f0-9]{40}$$"`..HEAD --count)
-CURRENT_COMMIT=$(shell git rev-parse --short=7 HEAD)
+CURRENT_COMMIT=g$(shell git rev-parse --short=7 HEAD)
 OPERATOR_VERSION=$(VERSION_MAJOR).$(VERSION_MINOR).$(COMMIT_NUMBER)-$(CURRENT_COMMIT)
 
 OPERATOR_IMAGE=$(IMAGE_REGISTRY)/$(IMAGE_REPOSITORY)/$(IMAGE_NAME)
